47851023e7537883e4bda6f89f7e8ecab4f1e44b
christophe de saint leger <cdesaintleger@creavi.fr>
2012-03-23 14:27:44 +0100

    Release par socket -> depuis l'interface

--- application/users/controllers/Index.php
+++ application/users/controllers/Index.php
@@ -229,23 +229,42 @@ class Index extends Oscar_Front_Controller{
         if( $this->CPOST_id_mail != null ){
 
             //Récupére les infos du mail à débloquer
-            $M_spam =   new M_msgs();
+            //$M_spam =   new M_msgs();
 
             $Tinfos =   $M_spam->get_infos( $_POST['id_mail'] );
-
-            $M_toRelease    =   new M_toRelease();
-
-            if(!$M_toRelease->alreadyLoaded($_POST['id_mail'])){
-
-                $M_toRelease->mail_id   =   $Tinfos['mail_id'];
-                $M_toRelease->quar_loc  =  $Tinfos['quar_loc'];
-                $M_toRelease->released  =   0;
-
-                $M_toRelease->createRecord();
-                unset($M_toRelease);
-
+            
+            
+            
+            $fp = fsockopen('10.0.0.3', 9998, $errno, $errstr, 30);
+            if (!$fp) {
+                echo "$errstr ($errno)<br />\n";
+            } else {
+                    $in = "request=release\r\n";
+                    $in .= "mail_id=".$Tinfos['mail_id']."\r\n";
+                    $in .= "secret_id=".$Tinfos['secret_id']."\r\n";
+                    $in .= "quar_type=".$Tinfos['quar_type']."\r\n";
+                    $in .= "mail_file=".$Tinfos['quar_loc']."\r\n";
+                    //$in .= "recipient=<$recipient>\r\n";
+                    $in .= "\r\n";
+            
+                fwrite($fp, $in);
+                fclose($fp);
             }
-            unset($M_spam);
+            
+            
+            //$M_toRelease    =   new M_toRelease();
+            //
+            //if(!$M_toRelease->alreadyLoaded($_POST['id_mail'])){
+            //
+            //    $M_toRelease->mail_id   =   $Tinfos['mail_id'];
+            //    $M_toRelease->quar_loc  =  $Tinfos['quar_loc'];
+            //    $M_toRelease->released  =   0;
+            //
+            //    $M_toRelease->createRecord();
+            //    unset($M_toRelease);
+            //
+            //}
+            //unset($M_spam);
 
 
         }
