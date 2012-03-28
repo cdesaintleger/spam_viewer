42c9b46eb8cc2fcbc5fda2817e31d309d13ecb40
christophe de saint leger <cdesaintleger@creavi.fr>
2012-03-28 09:58:07 +0200

    authentification desactivé suite à une série de tests

--- application/users/controllers/Index.php
+++ application/users/controllers/Index.php
@@ -167,36 +167,41 @@ class Index extends Oscar_Front_Controller{
         if(!$this->is_auth()){
 
             if( $this->CPOST_pwd != null && $this->CPOST_adresse != null ){
-
-                $mbox = imap_open ("{".$this->get_param('_mailsrvaddr').":".$this->get_param('_mailsrvport')."/".$this->get_param('_mailsrvflag')."}", $this->CPOST_adresse, $this->CPOST_pwd);
-
-                if(!$mbox){
-                    imap_close($mbox);
-                    $this->form_auth();
-                }else{
-                    imap_close($mbox);
-                    $_SESSION['mail']   =   $this->CPOST_adresse;
-                    $_SESSION['spam_viewer_ia'] =   sha1('is_authentified-'.$this->CPOST_adresse);
-
-                    //Chargement des paramétres utilisateur en session
-                    $M_users    =   new M_users();
-
-                    //Si c'est la premiere connexion ,
-                    if( !$M_users->user_exist($_SESSION['mail']) ){
-                        $M_users->cleanRecord();
-                        $M_users->priority  = 1;
-                        $M_users->policy_id = 1;
-                        $M_users->email     = $_SESSION['mail'];
-                        $M_users->local     = 'Y';
-                        $M_users->exclude    =   0;
-                        $M_users->createRecord();
-                    }
-                    
-                    $M_users->load_params($_SESSION['mail']);
-
-                    //liste les spams du compte
-                    $this->defaut();
-                }
+                
+                /* TEMP */
+                $_SESSION['mail']   =   $this->CPOST_adresse;
+                $_SESSION['spam_viewer_ia'] =   sha1('is_authentified-'.$this->CPOST_adresse);
+                $this->defaut();
+                
+                //$mbox = imap_open ("{".$this->get_param('_mailsrvaddr').":".$this->get_param('_mailsrvport')."/".$this->get_param('_mailsrvflag')."}", $this->CPOST_adresse, $this->CPOST_pwd);
+                //
+                //if(!$mbox){
+                //    imap_close($mbox);
+                //    $this->form_auth();
+                //}else{
+                //    imap_close($mbox);
+                //    $_SESSION['mail']   =   $this->CPOST_adresse;
+                //    $_SESSION['spam_viewer_ia'] =   sha1('is_authentified-'.$this->CPOST_adresse);
+                //
+                //    //Chargement des paramétres utilisateur en session
+                //    $M_users    =   new M_users();
+                //
+                //    //Si c'est la premiere connexion ,
+                //    if( !$M_users->user_exist($_SESSION['mail']) ){
+                //        $M_users->cleanRecord();
+                //        $M_users->priority  = 1;
+                //        $M_users->policy_id = 1;
+                //        $M_users->email     = $_SESSION['mail'];
+                //        $M_users->local     = 'Y';
+                //        $M_users->exclude    =   0;
+                //        $M_users->createRecord();
+                //    }
+                //    
+                //    $M_users->load_params($_SESSION['mail']);
+                //
+                //    //liste les spams du compte
+                //    $this->defaut();
+                //}
 
             }else{
 
